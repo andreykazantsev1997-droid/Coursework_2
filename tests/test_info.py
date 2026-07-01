15fe598342e914ac53a12a6063f69c011686633a
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-07-01 18:25:08 +0300

    add new test for 70%

--- tests/test_info.py
+++ tests/test_info.py
@@ -10,3 +10,9 @@ def test_api_adapter_url():
     api = APIAdapter()
     assert "openstreetmap" in api.openstreetmap_url
     assert "opensky-network" in api.opensky_url
+
+def test_api_adapter_connect_error_branch():
+    api = APIAdapter()
+    api.openstreetmap_url = "https://broken-url-that-does-not-exist-12345.org"
+    result = api.connect()
+    assert result is False
